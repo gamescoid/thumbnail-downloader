c61632552fa4736fdf7b933b1b9258fe00dbe9a7
donisaguling <donisaguling@gmail.com>
2024-09-03 18:12:16 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Just clone this project and run index.html.
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [
+    
+  ],
+  daisyui: {
+    themes: ["winter"],
+  },
+}
